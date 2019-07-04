29092dd72e27a0ded6ff07539d8dbcf2ba0ab7d4
ToNy2198 <52449321+ToNy2198@users.noreply.github.com>
2019-07-04 14:08:04 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function openSide() {
+    document.getElementById("side-list-js").style.width ="300px";
+//		document.getElementById("mainbox").style.marginLeft="300px";
+}
+function closeSide(){
+    document.getElementById("side-list-js").style.width ="0px";
+//			document.getElementById("mainbox").innerHTML="&#9776;";
+}
